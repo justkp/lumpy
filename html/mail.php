cc7e05f69b964f8adfb523578b48abaeace5459d
justkp <justkp@gmail.com>
2014-06-25 00:47:52 -0400

    temp php form post

---
+++ html/mail.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Email: $email \n Message: $message";
+$recipient = "melissa.tung@gmail.com";
+$subject = "MelissaTung.com Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+header("Location: http://www.melissatung.com");
+?>
